750326f908a4d69aa3e200dd5ebf21bbcdca70f5
ghorikahan <kahan.ghori.cg@gmail.com>
2025-12-04 13:16:15 +0530

    nothing chenges

--- Clanguage_practical/Q1.c
+++ Clanguage_practical/Q1.c
@@ -1,19 +1,18 @@
-#include<stdio.h>
-int main(){
-    int mat[3][3]={
-        {1,2,3},
-        {4,5,6},
-        {7,8,9}
-    };
-    int i,sum=0;
-    
-    for ( i = 1; i <=3; i++)
+#include <stdio.h>
+int main()
+{
+    int mat[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}};
+    int i, sum = 0;
+
+    for (i = 0; i < 3; i++)
     {
-        sum = sum+mat[2][i];
+        sum = sum + mat[1][i];
     }
 
-    printf("%d",sum);
-    
+    printf("%d", sum);
 
     return 0;
 }
